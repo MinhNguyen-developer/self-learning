f49802f472b966b8dc1e8fa0525eb8be97a0e307
MinhNguyen-developer <85623347+MinhNguyen-developer@users.noreply.github.com>
2021-10-12 10:00:31 +0700

    escape

---
+++ switch-case/main.js
@@ -0,0 +1,19 @@
+var date = 2;
+
+switch (date) {
+  case 2:
+    console.log("monday");
+    break;
+
+  case 3:
+    console.log("tuesday");
+    break;
+
+  case 4:
+    console.log("wednesday");
+    break;
+
+  case 5:
+    console.log("thursday");
+    break;
+}
