adcb95b8be6bae752ce90382edf49e04b92e192d
MinhNguyen-developer <85623347+MinhNguyen-developer@users.noreply.github.com>
2021-10-12 09:41:24 +0700

    escape

--- date-object/index.js
+++ date-object/index.js
@@ -2,4 +2,8 @@
 
 var date = new Date();
 
-console.log(date);
+var year = date.getFullYear();
+var month = date.getMonth() + 1;
+var day = date.getDate();
+
+console.log(`${day}/${month}/${year}`);
