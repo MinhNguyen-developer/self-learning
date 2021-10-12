e6ef29aad98f52ddc76bf7fb69cac9ea8fed2b58
MinhNguyen-developer <85623347+MinhNguyen-developer@users.noreply.github.com>
2021-10-12 10:07:35 +0700

    escape

---
+++ ternary operator/main.js
@@ -0,0 +1,17 @@
+var course = {
+  name: "Javascript",
+  coin: 250,
+};
+
+// if (course.coin > 0) {
+//   console.log(`${course.coin} Coins`);
+// } else {
+//   console.log("Free");
+// }
+
+const result = course.coin > 0 ? `${course.coin} Coins` : "Free";
+
+var a = 1;
+var b = 2;
+
+var c = a > 0 ? a : b;
